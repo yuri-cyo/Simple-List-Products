ac2edecf4db44ddd9751aa532e84f53655fc9375
Yuri Chapyuk <yurachapyuk@gmail.com>
2023-07-01 00:08:15 +0300

    start template for electron-vite-vue-sass

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+// import sassPlugin from 'vite-plugin-sass';
+// const sassPlugin = require('vite-plugin-sass')
+// import vueJsx from '@vitejs/plugin-vue-jsx';
+import electron from 'vite-plugin-electron';
+import renderer from 'vite-plugin-electron-renderer';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // sassPlugin(),
+    electron([
+      {
+        // Main-Process entry file of the Electron App.
+        entry: 'electron/main.ts',
+      },
+      {
+        entry: 'electron/preload.ts',
+        onstart(options) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+          // instead of restarting the entire Electron App.
+          options.reload();
+        },
+      },
+    ]),
+    renderer(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/styles/variables.scss';`, // This is optional if you need to import additional Sass files
+      },
+    },
+  },
+});
+
+// 
